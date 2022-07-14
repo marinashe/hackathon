60858d5ce28fae783740579066a79ed17c90e505
Jonathan Carse <jonathan@vhive.ai>
2022-07-14 11:20:33 +0300

    support GET single ticket, add title to ticket mock

--- server/index.js
+++ server/index.js
@@ -8,6 +8,17 @@ app.get('/tickets', (req, res) => {
     res.json(tickets);
 })
 
+app.get('/tickets/:id', (req, res) => {
+    const requestId = Number(req.params.id);
+    const ticket = tickets.find(t => t.id === requestId);
+
+    if (ticket) {
+        res.json(ticket);
+    } else {
+        res.sendStatus(404).send('Not found');
+    }
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
